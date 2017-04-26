95c7ec0f59ecf669214218aa65eccb7954c048a8
Guilherme <gui.msr.98@gmail.com>
2017-04-25 21:48:18 -0300

    remoção pasta duplicada

--- bancocentral/php/deletar.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include './connect_bd.php';
-
-mysql_query("DELETE FROM {$_GET['tabela']} FROM ID = {$_GET['id']}");
-
-header("index.php?pagina={$_GET['voltar']}&mensagem=Registro Excluido com Sucesso");
